cf1e2827f1f1144e111d77349bcded6b6756ed8e
Bradley <57405760+Bradley00Uni@users.noreply.github.com>
2021-06-01 02:20:50 +0100

    Version 1.6.1
    > Additional Styling
    > Removal of small, unnecessary features

---
+++ ITS_Support/ITS_Support/Controllers/TestingController.cs
@@ -0,0 +1,35 @@
+﻿using ITS_Support.Data;
+using ITS_Support.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace ITS_Support.Controllers
+{
+    public class TestingController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TestingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            TestingViewModel testingModel = new TestingViewModel()
+            {
+                GeneralTickets = await _context.GeneralTickets.ToListAsync(),
+                TechnicalTickets = await _context.TechnicalTickets.ToListAsync(),
+                RoomTickets = await _context.RoomTickets.ToListAsync(),
+                Campuses = await _context.Campuses.ToListAsync(),
+                Rooms = await _context.Rooms.ToListAsync(),
+                Assets = await _context.Assets.ToListAsync()
+            };
+
+            return View(testingModel);
+        }
+        
+    }
+}
---
+++ ITS_Support/ITS_Support/Models/Testing/TestingViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITS_Support.Models
+{
+    public class TestingViewModel
+    {
+        public IEnumerable<GeneralTicketModel> GeneralTickets { get; set; }
+        public IEnumerable<TechnicalTicketModel> TechnicalTickets { get; set; }
+        public IEnumerable<RoomTicketModel> RoomTickets { get; set; }
+        public IEnumerable<RoomModel> Rooms { get; set; }
+        public IEnumerable<CampusModel> Campuses { get; set; }
+        public IEnumerable<AssetModel> Assets { get; set; }
+    }
+}
